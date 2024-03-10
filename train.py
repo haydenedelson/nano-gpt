599e18a318d7a0e218b9b3753a9ab807f8ac9376
haydenedelson <59719221+haydenedelson@users.noreply.github.com>
2024-03-10 16:29:52 -0400

    Update resume (#6)
    * Fix resume bug
    
    * Update config

--- train.py
+++ train.py
@@ -75,6 +75,9 @@ def main(cfg):
     save_dir = os.path.join(cfg.logging.output_dir, cfg.experiment_name)
     os.makedirs(save_dir, exist_ok=True)
 
+    run.config = cfg
+    run.update()
+
     # Load data
     with open(os.path.join(hydra.utils.get_original_cwd(), cfg.data.file_path), 'r', encoding='utf-8') as in_file:
         text = in_file.read()
@@ -120,8 +123,9 @@ def main(cfg):
     print("Loss:", loss_fn)
 
     # Resume training state if state artifact is specified
+    curr_epoch = 0
     if 'resume_state_artifact' in cfg and cfg.resume_state_artifact is not None:
-        model, optimizer, scheduler, epoch = utils.load_train_state(run,
+        model, optimizer, scheduler, curr_epoch = utils.load_train_state(run,
                                                                     cfg.resume_state_artifact.path,
                                                                     cfg.resume_state_artifact.file,
                                                                     model,
@@ -131,7 +135,7 @@ def main(cfg):
     model_name = f"{cfg.model.name}_{cfg.model.params.embed_dim}_{cfg.model.params.num_layers}_{cfg.loss.name}_{cfg.optimizer.name}_{cfg.scheduler.name}"
     curr_best = None
     try:
-        for epoch in tqdm(range(cfg.num_epochs)):
+        for epoch in tqdm(curr_epoch, range(cfg.num_epochs)):
             # Run validation step first per epoch
             torch.set_grad_enabled(False)
             model.eval()
